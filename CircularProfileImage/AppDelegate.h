eb22f36e9402390b2a681c09d3958824d394ef7a
vickyappmaker <convergence@Convergences-Mac-mini.local>
2015-03-05 17:01:01 -0800

    Initial Commit

---
+++ CircularProfileImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CircularProfileImage
+//
+//  Created by Convergence on 05/03/15.
+//  Copyright (c) 2015 Convergence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
